f46a21d943a35c2f9df4f7f0b80bddbaf4bf723b
Cethric <cethric@users.noreply.github.com>
2026-06-14 22:53:26 +1000

    Revert "fix: github code quality"
    This reverts commit e90857d851e51991733389b32012b2ec8ac243b5.

--- packages/components/src/components/base-form-builder/base-form-builder-properties.vue
+++ packages/components/src/components/base-form-builder/base-form-builder-properties.vue
@@ -27,15 +27,6 @@
 
   const props = withDefaults(
     defineProps<{
-      field: BuilderField;
-      fieldTypes: FieldTypeDescriptor[];
-      disabled?: boolean;
-      keyError?: string;
-      wizard?: boolean;
-      stepCount?: number;
-      step?: number;
-    }>(),
-    {
       disabled: false,
       keyError: undefined,
       wizard: false,
